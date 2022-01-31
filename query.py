6ff6594847efc098ba38d79b20e33a75c5ae1861
Nick Misiiuk <mykyta.misiiuk@gmail.com>
2022-01-31 10:48:00 +0000

    improved DataFrame Query method

--- query.py
+++ query.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def query(filename, types, certainties=[0.9]):
+def query(filename, types, certainties=[0.9], columns=['dr7objid', 'ra', 'dec']):
     """
     Basic query for Galaxy Zoo 2 data.
     """
@@ -8,13 +8,13 @@ def query(filename, types, certainties=[0.9]):
     if ".csv" in filename:
         name = filename
     else:
-        name = "%s.csv" %(filename)
+        name = "%s.csv" % (filename)
 
-    loaded_file = pd.read_csv(r'%s' %(name))
+    loaded_file = pd.read_csv(r'%s' % (name))
 
     while len(certainties) < len(types):
         certainties.append(certainties[0])
-    
+
     gz2_col = []
 
     for type in types:
@@ -23,17 +23,20 @@ def query(filename, types, certainties=[0.9]):
         elif type[0] == "S":
             gz2_col.append("t03_bar_a06_bar_debiased")
 
-    df = pd.DataFrame(loaded_file, columns=['dr7objid', 'ra', 'dec', 'gz2_class', 't01_smooth_or_features_a01_smooth_debiased','t03_bar_a06_bar_debiased'])
+    df = pd.DataFrame(loaded_file, columns=columns)
     #df_f = df[df.gz2_class.isin(types)]
 
     dff = pd.DataFrame([])
 
     for x in range(len(types)):
-        dft = df.query('gz2_class.str.startswith("%s") and %s > %s' %(types[x], gz2_col[x], certainties[x]), engine="python")
+        dft = df.query('gz2_class.str.startswith("%s") and %s > %s' % (
+            types[x], gz2_col[x], certainties[x]), engine="python")
 
-        dff = pd.concat([dff,dft])
-    
+        dff = pd.concat([dff, dft])
+
     return(dff)
-    
 
-#print(query("gz2_hart16",certainties=[0.95,0.2],types=["E"]))
+
+result = query("gz2_hart16", certainties=[0.95, 0.2], types=["E"], columns=[
+               'dr7objid', 'ra', 'dec', 'gz2_class', 't01_smooth_or_features_a01_smooth_debiased', 't03_bar_a06_bar_debiased'])
+print(result)
