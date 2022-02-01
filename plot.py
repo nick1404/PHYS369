4f96b4e76f613d530405cd055862f6094d09dc79
Nick Misiiuk <mykyta.misiiuk@gmail.com>
2022-02-01 16:07:46 +0000

    Improved Plot function

--- plot.py
+++ plot.py
@@ -1,10 +1,8 @@
 import matplotlib as mpl
 from matplotlib import pyplot as plt
-from matplotlib import colors as mcolors
-from mpl_toolkits import mplot3d
-
 
 def plotstart(data, plot_type):
+
     if len(data[0]) > 3 or len(data[0]) < 2:
         print("Too much or not enough data!")
         return
@@ -26,26 +24,20 @@ def plotstart(data, plot_type):
     plt.figure(dpi=200)
 
     ax = plt.axes(projection=proj)
-    ax.set_xlabel(data[0][0][0])
-    ax.set_ylabel(data[0][1][0])
+    ax.set_xlabel(data[0][0].name)
+    ax.set_ylabel(data[0][1].name)
+    
     if proj == "3d":
         ax.set_zlabel(data[0][2][0])
 
     for x in range(len(data)):
-        for y in range(len(data[x])):
-            del data[x][y][0]
 
         if plot_type == "line":
             plt.plot(*data[x], colours[x], label="Data %s" % (x))
+            
         elif plot_type == "scatter":
             plt.scatter(*data[x], marker=".", c=colours[x],
                         label="Data %s" % (x))
 
-    plt.legend(loc="upper left")
-
-
-data = [[["x", 1, 2, 3, 4, 5], ["y", 1, 2, 3, 4, 5], ["z", 1, 2, 3, 4, 5]],
-        [["x", 5, 6, 7, 8, 9], ["y", 5, 6, 7, 8, 9], ["z", 5, 6, 7, 8, 9]]]
-
-plotstart(data, "scatter")
-plt.show()
+    plt.legend()
+    plt.show()
